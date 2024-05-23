9293210a94e03ba0bbe1bc046efe5017e5b69993
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-05-23 15:05:50 +0100

    feat: subscribe button done and completed

--- src/sections/Subscribe.tsx
+++ src/sections/Subscribe.tsx
@@ -1,24 +1,36 @@
-
+import EmailIcon from "../assets/SVG/email.svg";
+import EmailSend from "../assets/SVG/email-send.svg";
 
 export default function Subscribe() {
   return (
-    <section className="relative z-10 flex items-center justify-center p-4 bg-white mt-8 sm:mt-40">
-  <div className="absolute inset-0 flex items-center justify-center">
-    <div className="relative text-center w-full max-w-lg p-4 sm:p-6 bg-white rounded-lg border border-black-500">
-      <div className="absolute inset-0 shadow-md rounded-lg" style={{ top: '-0.5rem' }}></div>
-      <h2 className="text-lg sm:text-xl font-semibold mb-2">Subscribe to get News and Updates</h2>
-      <p className="text-sm sm:text-base text-gray-700">Want to know about what we’re up to, news about Africa, and updates with our app? Sign up for our newsletter!</p>
-    </div>
-  </div>
-</section>
-    // <section className="relative z-10 flex items-center justify-center p-4 bg-white mt-40">
-    //   <div className="absolute inset-0 flex items-center justify-center">
-    //     <div className="relative text-center w-full max-w-xl p-6 bg-white rounded-lg border border-black-500">
-    //       <div className="absolute inset-0 shadow-md rounded-lg" style={{ top: '-0.5rem' }}></div>
-    //       <h2 className="text-xl font-semibold mb-2">Subscribe to get News and Updates</h2>
-    //       <p className="text-gray-700">Want to know about what we’re up to, news about Africa, and updates with our app? Sign up for our newsletter!</p>
-    //     </div>
-    //   </div>
-    // </section>
-  )
+    <section className="relative z-10 flex items-center justify-center bg-white mt-36 lg:mt-40">
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="relative text-center w-full lg:w-[716px] p-4 sm:p-6  bg-white rounded-xl border border-black-500 shadow-2xl">
+          <h2 className="text-lg sm:text-xl font-semibold mb-2 mt-8">
+            Subscribe to get News and Updates
+          </h2>
+          <p className="text-sm sm:text-base text-gray-700 lg:w-[620px]">
+            Want to know about what we’re up to, news about Africa, and updates
+            with our app? Sign up for our newsletter!
+          </p>
+          <div className="flex justify-center items-center gap-2">
+            <div className="relative mt-6">
+              <span className="absolute inset-y-0 flex items-center pl-3">
+                <img src={EmailIcon} alt="email icom" />
+              </span>
+              <input
+                type="text"
+                title="email"
+                placeholder="Email Address"
+                className="w-full pl-10 border py-2 pr-4 rounded-lg focus:outline-none focus:border-blue-500 hover:ring-0"
+              />
+            </div>
+            <div className="mt-5 w-10">
+              <img src={EmailSend} alt="email icon" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
