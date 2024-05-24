db175afe4f6f1d94565a7cad895d0e33f3b23635
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-05-24 12:20:23 +0100

    fix: fixed pushed now

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,8 @@ import { Footer, ComingSoon,  Org, Subscribe } from "./sections/Index";
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
-      {/* <Hero /> */}
       <Org />
       <ComingSoon />
-      {/* <Faqs /> */}
       <Subscribe />
       <Footer />
     </>
