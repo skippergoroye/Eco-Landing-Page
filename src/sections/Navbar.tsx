bd1c78ea0927748e747e652e96063631e49a41a7
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-05-26 18:34:05 +0100

    fix: moved to the right

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <section className="px-8 lg:px-16 py-12 mx-auto max-w-[144opx]">
       <div className="bg-white rounded-lg shadow-3xl border z-10 border-blu">
-        <div>
+        <div className="lg:ml-8 ml-2">
           <img src={NavLogo} />
         </div>
       </div>
