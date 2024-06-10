d6ad7993ba986c1ec2a9f970aee8dfd12707cbb8
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-06-10 21:32:23 +0100

    feat: double style removed

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -4,8 +4,8 @@ import Avater from "../assets/SVG/Avatar-Image.svg";
 
 export default function Navbar() {
   return (
-    <section className="py-6 mb-36 fixed  z-50">
-      <div className="fixed w-full px-8 lg:px-16 bg-gray-50">
+    <section className="py-6 mb-36 fixed z-50">
+      <div className="px-8 lg:px-16 bg-gray-50">
         <div className="flex items-center gap-1 lg:gap-4 shadow-lg px-2 lg:px-4 rounded-xl">
           <img src={NavLogo} alt="header" />
           <div className="flex">
