5c136999bff91be37913a6d456a2ddc6c03d3b6b
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-06-14 10:02:05 +0100

    feat: bordr fixed for input field

--- src/sections/Subscribe.tsx
+++ src/sections/Subscribe.tsx
@@ -22,7 +22,7 @@ export default function Subscribe() {
                 type="text"
                 title="email"
                 placeholder="Email Address"
-                className="w-full pl-10 border py-2 pr-4 rounded-lg focus:outline-none focus:border-blue-500 hover:ring-0"
+                className="w-full pl-10 py-2 pr-4 rounded-lg border border-black focus:outline-none focus-visible:ring-0"
               />
             </div>
             <div className="mt-5 w-10">
